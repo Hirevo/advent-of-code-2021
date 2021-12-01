47d3ce1d47b48d3fe232b000473cf90d1904c168
Nicolas Polomack <nicolas@polomack.eu>
2021-12-02 00:53:12 +0100

    refactor: minor day01 refactoring

--- src/day01.rs
+++ src/day01.rs
@@ -7,16 +7,16 @@ pub const INPUT_PATH: &str = "inputs/day01.txt";
 pub fn run() -> Result<(), Error> {
     let input = fs::read_to_string(INPUT_PATH)?;
 
-    let measures: Vec<isize> = input
+    let measures: Vec<usize> = input
         .lines()
         .map(|line| line.trim().parse())
         .collect::<Result<_, _>>()?;
 
-    let part1 = measures.windows(2).filter(|w| w[1] - w[0] > 0).count();
+    let part1 = measures.windows(2).filter(|w| w[0] < w[1]).count();
     println!("part1: {}", part1);
 
-    let sums: Vec<isize> = measures.windows(3).map(|w| w.into_iter().sum()).collect();
-    let part2 = sums.windows(2).filter(|w| w[1] - w[0] > 0).count();
+    let sums: Vec<usize> = measures.windows(3).map(|w| w.into_iter().sum()).collect();
+    let part2 = sums.windows(2).filter(|w| w[0] < w[1]).count();
     println!("part2: {}", part2);
 
     Ok(())
