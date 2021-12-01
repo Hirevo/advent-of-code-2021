8565dbe14343be629730ac9088ed53718326cd0e
Nicolas Polomack <nicolas@polomack.eu>
2021-12-01 15:13:55 +0100

    feature: added day01

--- src/day01.rs
+++ src/day01.rs
@@ -5,9 +5,19 @@ use crate::Error;
 pub const INPUT_PATH: &str = "inputs/day01.txt";
 
 pub fn run() -> Result<(), Error> {
-    let _input = fs::read_to_string(INPUT_PATH)?;
+    let input = fs::read_to_string(INPUT_PATH)?;
 
-    todo!();
+    let measures: Vec<isize> = input
+        .lines()
+        .map(|line| line.trim().parse())
+        .collect::<Result<_, _>>()?;
+
+    let part1 = measures.windows(2).filter(|w| w[1] - w[0] > 0).count();
+    println!("part1: {}", part1);
+
+    let sums: Vec<isize> = measures.windows(3).map(|w| w.into_iter().sum()).collect();
+    let part2 = sums.windows(2).filter(|w| w[1] - w[0] > 0).count();
+    println!("part2: {}", part2);
 
     Ok(())
 }
