e2e1c0881c084855013739f66f12f353cff99088
Nicolas Polomack <nicolas@polomack.eu>
2021-12-06 20:43:42 +0100

    feature: added Day06

--- src/day06.rs
+++ src/day06.rs
@@ -4,19 +4,49 @@ use crate::Error;
 
 pub const INPUT_PATH: &str = "inputs/day06.txt";
 
-fn part1(_input: &str) -> Result<(), Error> {
-    todo!()
+fn part1(input: &[usize]) -> Result<(), Error> {
+    let mut groups = [0usize; 9];
+
+    input.iter().for_each(|it| {
+        groups[*it] += 1;
+    });
+
+    (0..80).for_each(|_| {
+        groups.rotate_left(1);
+        groups[6] += groups[8];
+    });
+
+    println!("part1: {}", groups.into_iter().sum::<usize>());
+    Ok(())
 }
 
-fn part2(_input: &str) -> Result<(), Error> {
-    todo!();
+fn part2(input: &[usize]) -> Result<(), Error> {
+    let mut groups = [0usize; 9];
+
+    input.iter().for_each(|it| {
+        groups[*it] += 1;
+    });
+
+    (0..256).for_each(|_| {
+        groups.rotate_left(1);
+        groups[6] += groups[8];
+    });
+
+    println!("part2: {}", groups.into_iter().sum::<usize>());
+    Ok(())
 }
 
 pub fn run() -> Result<(), Error> {
     let input = fs::read_to_string(INPUT_PATH)?;
 
-    part1(input.as_str())?;
-    part2(input.as_str())?;
+    let input: Vec<usize> = input
+        .trim()
+        .split(",")
+        .map(|it| it.parse())
+        .collect::<Result<_, _>>()?;
+
+    part1(input.as_slice())?;
+    part2(input.as_slice())?;
 
     Ok(())
 }
