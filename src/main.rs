346b70129504ed6fe660d2dd71f0263a1da91891
Nicolas Polomack <nicolas@polomack.eu>
2021-12-07 23:09:49 +0100

    fix: changed program behavior for yet unsolved days

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,6 @@ mod day04;
 mod day05;
 mod day06;
 mod day07;
-mod day08;
-mod day09;
-mod day10;
-mod day11;
-mod day12;
-mod day13;
-mod day14;
-mod day15;
-mod day16;
-mod day17;
-mod day18;
-mod day19;
-mod day20;
-mod day21;
-mod day22;
-mod day23;
-mod day24;
 
 pub type Error = Report;
 
@@ -51,23 +34,10 @@ fn main() -> Result<(), Error> {
         5 => day05::run(),
         6 => day06::run(),
         7 => day07::run(),
-        8 => day08::run(),
-        9 => day09::run(),
-        10 => day10::run(),
-        11 => day11::run(),
-        12 => day12::run(),
-        13 => day13::run(),
-        14 => day14::run(),
-        15 => day15::run(),
-        16 => day16::run(),
-        17 => day17::run(),
-        18 => day18::run(),
-        19 => day19::run(),
-        20 => day20::run(),
-        21 => day21::run(),
-        22 => day22::run(),
-        23 => day23::run(),
-        24 => day24::run(),
+        n if (1..=24).contains(&n) => {
+            println!("this day has not (yet) been solved !");
+            Ok(())
+        }
         _ => {
             eprintln!("invalid day number, it must be within the range [1,24].");
             std::process::exit(1);
